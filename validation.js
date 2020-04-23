a81a5c0b17a1674534402f7746fd326789233725
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-23 18:45:48 +0100

    Set border to red on validation error

--- validation.js
+++ validation.js
@@ -1,4 +1,5 @@
-//Prevent default error handling
+//show border on blur
+//show icon on blur
 //show message on blur
 //show correct message
 //remove message when error fixed
@@ -6,3 +7,24 @@
 
 const form = document.querySelector("form");
 form.setAttribute("novalidate", true);
+
+const hasError = (field) => {
+  if (field.validity.valid) return;
+  if (field.validity.valueMissing) {
+    return `${field.placeholder} cannot be empty`;
+  }
+};
+
+const showError = (field, error) => {
+  const parentFormField = field.parentElement;
+  parentFormField.classList.add("error");
+};
+
+document.addEventListener(
+  "blur",
+  (event) => {
+    const error = hasError(event.target);
+    if (error) showError(event.target, error);
+  },
+  true
+);
