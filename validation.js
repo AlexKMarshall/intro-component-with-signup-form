5ebc3c8c3566506294d16f37ce1120b224c8d945
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-23 18:18:24 +0100

    Set form to novalidate in script

---
+++ validation.js
@@ -0,0 +1,8 @@
+//Prevent default error handling
+//show message on blur
+//show correct message
+//remove message when error fixed
+//prevent submission if form invalid
+
+const form = document.querySelector("form");
+form.setAttribute("novalidate", true);
